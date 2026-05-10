e7962ba3bf4b27d7b2402510d5fff59d62daeb21
raihan-jishan <raihanjishan3242@gmail.com>
2026-05-10 22:36:07 +0600

    first commit

--- app/quotes/page.jsx
+++ app/quotes/page.jsx
@@ -6,7 +6,7 @@ import { QuoteCard } from "@/components/ui/quoteCard";
 import { Heading } from "@/components/ui/heading";
 import { QuotesAPI } from "@/lib/api";
 import { QuoteCardSkeleton, Skeleton } from "@/skeleton";
-const page = () => {
+const Quotes = () => {
   const [quotes, setQuotes] = useState([]);
   const searchParams = useSearchParams();
   const [loading, setLoading] = useState(true);
@@ -87,4 +87,4 @@ const page = () => {
     </>
   );
 };
-export default page;
+export default Quotes;
